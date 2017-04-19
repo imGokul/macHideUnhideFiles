3557e173d29f83d3c590d2bb13c8551fba94f50f
Gokul <sgokulkrishnan@gmail.com>
2017-04-19 19:07:05 +0530

    Show Hide files on click with notification

--- macHideUnhideFiles/macHideUnhideFiles/AppDelegate.m
+++ macHideUnhideFiles/macHideUnhideFiles/AppDelegate.m
@@ -10,7 +10,6 @@
 
 @interface AppDelegate ()
 
-@property (strong, nonatomic) IBOutlet NSMenu *statusMenu;
 @property (strong, nonatomic) NSStatusItem *statusItem;
 
 @end
@@ -19,19 +18,50 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    [self.statusItem setMenu:self.statusMenu];
-    [self.statusItem setTitle:@"..."];
     [self.statusItem setHighlightMode:YES];
+    NSImage *img = [NSImage imageNamed:@"FolderIcon"];
+    [self.statusItem setImage:img];
+    [self.statusItem setAction:@selector(menuClicked:)];
+}
+
+- (void)menuClicked:(id)sender {
+    //Below code is to quit app on control click
+    NSEvent *event = [NSApp currentEvent];
+    if([event modifierFlags] & NSEventModifierFlagControl) {
+        [[NSApplication sharedApplication] terminate:self];
+        return;
+    }
+    
+    //update hidden files status
+    Boolean show = false;
+    Boolean showAllFiles = CFPreferencesGetAppBooleanValue(CFSTR("AppleShowAllFiles"), CFSTR("com.apple.finder"), &show);
+    if(showAllFiles){
+        CFPreferencesSetAppValue(CFSTR("AppleShowAllFiles"), kCFBooleanFalse, CFSTR("com.apple.finder"));
+    }else{
+        CFPreferencesSetAppValue(CFSTR("AppleShowAllFiles"), kCFBooleanTrue, CFSTR("com.apple.finder"));
+    }
+    [[NSTask launchedTaskWithLaunchPath:@"/usr/bin/killall" arguments:@[@"Finder", @"/System/Library/CoreServices/Finder.app"]] waitUntilExit];
+    showAllFiles = CFPreferencesGetAppBooleanValue(CFSTR("AppleShowAllFiles"), CFSTR("com.apple.finder"), &show);
+    NSString *notifString;
+    if(showAllFiles){
+        notifString = @"All files will be dispalyed now";
+    }else
+    {
+        notifString = @"Hidden files are safe and not visible";
+    }
+    NSUserNotification *notification = [[NSUserNotification alloc] init];
+    notification.title = @"Maṟaikka";
+    notification.informativeText = notifString;
+    notification.soundName = NSUserNotificationDefaultSoundName;
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+}
+
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center shouldPresentNotification:(NSUserNotification *)notification
+{
+    return YES;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-}
-
-- (IBAction)ShowHiddenFiles{
-}
-
-
-- (IBAction)HideHiddenFiles{
 }
 
 @end
